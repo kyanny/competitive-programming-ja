966c8fe79849c4e958a1c9b2c8c170f3f1d33876
Kensuke Nagae <kyanny@gmail.com>
2019-12-21 03:32:43 +0900

    Impl after reading answer explanation

--- abc046/abc046_b/main.rb
+++ abc046/abc046_b/main.rb
@@ -1,18 +1,3 @@
 N, K = gets.chomp.split.map(&:to_i)
-=begin
-K! / (K-N)!
-=end
 
-def factorial(n)
-  # return 1 if n == 0
-  # n * kaijou(n - 1)
-  1.upto(n).inject { |acc, i| acc *= i }
-end
-
-puts factorial(N) / factorial(N-K)
-
-# p factorial(K)
-
-# x = [K - N, K].max
-# p factorial(x)
-# puts factorial(K) / factorial(x)
+puts K * ((K - 1) ** (N - 1))
